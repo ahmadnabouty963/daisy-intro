43a1ab6e1aa3ed50b8f047d036b4e89e94238c9d
AhmadNabouty <ahmad.nabouty@dci-student.org>
2026-04-02 01:57:21 +0200

    fix: remove duplicate stylesheet link in travel.html and format vite.config.js for consistency

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	plugins: [tailwindcss()],
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+
+        travel: resolve(__dirname, "travel.html"),
+      },
+    },
+  },
 });
